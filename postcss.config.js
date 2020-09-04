58931ba5a67ffb2c0d41dc6e1891fd3335849ffc
Chris Ellis <ellis.cs@gmail.com>
2020-09-04 11:25:46 -0400

    CSS for Next

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,19 @@
+
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  // Specify the paths to all of the template files in your project
+  content: ['./pages/**/*.js', './components/**/*.js'],
+
+  // make sure css reset isnt removed on html and body
+  whitelist: ['html', 'body'],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
 module.exports = {
-  plugins: ['tailwindcss', 'postcss-preset-env'],
-}
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])
+  ]
+};
