c106a8858810ff672594659ab09e503112cc2f97
Chris Ellis <ellis.cs@gmail.com>
2020-09-08 16:14:45 -0400

    Purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,15 @@
-// postcss.config.js
+const purgecss = [
+  "@fullhuman/postcss-purgecss",
+  {
+    content: ["./components/**/*.js", "./pages/**/*.js"],
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+  }
+];
 module.exports = {
   plugins: [
-    'tailwindcss',
-    'autoprefixer'
-  ],
-}
+    "postcss-import",
+    "tailwindcss",
+    "autoprefixer",
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
+  ]
+};
