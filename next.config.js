ca75fdb63a8870b445c5d410c68408fd9d32525a
Chris Ellis <ellis.cs@gmail.com>
2020-09-08 19:11:16 -0400

    Rebase
    Update purge
    
    :(
    
    Gamify
    
    Remove timer
    
    Purgecss
    
    Import order
    
    Purge ignore
    
    Apply class instead of element

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
 module.exports = {
   /* config options here */
 }
+=======
+>>>>>>> f1d9d79... :(
+=======
+module.exports = {
+  /* config options here */
+}
+>>>>>>> c106a88... Purgecss
--- postcss.config.js
+++ postcss.config.js
@@ -10,6 +10,6 @@ module.exports = {
     "postcss-import",
     "tailwindcss",
     "autoprefixer",
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : [purgecss])
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
   ]
 };
