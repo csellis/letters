13e7fcc78c057336c91b8bd49a2bbac604a57a04
Chris Ellis <ellis.cs@gmail.com>
2020-09-08 16:41:24 -0400

    Purge ignore

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import '../styles/index.css'
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- pages/index.js
+++ pages/index.js
@@ -53,7 +53,7 @@ export default function Home() {
   console.log(`Listening: ${listening}`)
 
   return (
-    <>
+    <div className="">
       <div className="flex flex-col justify-center items-center min-h-screen">
         <div className="top-0 right-0 text-3xl text-yellow-400 font-mono px-4 py-2 font-semibold">
           {score} points
@@ -72,6 +72,6 @@ export default function Home() {
         </div>
       </div>
 
-    </>
+    </div>
   );
 }
